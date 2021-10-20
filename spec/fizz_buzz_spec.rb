0030cf0be8356a8ba0b92e57b79a73921d9929b9
giacoletti <giacoletti92@gmail.com>
2021-10-20 11:19:31 +0200

    adds spec for FizzBuzz class

---
+++ spec/fizz_buzz_spec.rb
@@ -0,0 +1,22 @@
+describe FizzBuzz do
+
+    subject { FizzBuzz.new } #subject case
+
+    #test case
+    it "is expected to return a number if none of the game rules apply" do
+        expect(subject.check(1)).to eq 1
+    end
+
+    if "is expected to return 'fizz' if the number is divisible by 3" do
+        expect(subject.check(3)).to eq "fizz"
+    end
+
+    if "is expected to return 'buzz' if the number is divisible by 5" do
+        expect(subject.check(5)).to eq "buzz"
+    end
+
+    if "is expected to return 'fizz-buzz' if the number is divisible by 15" do
+        expect(subject.check(15)).to eq "fizz-buzz"
+    end
+
+end
