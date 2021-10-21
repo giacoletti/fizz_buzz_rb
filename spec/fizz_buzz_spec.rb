48fbb49ba65baa037709da637e73534b50f5ea43
giacoletti <giacoletti92@gmail.com>
2021-10-21 11:31:40 +0200

    solves the challenge

--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -12,11 +12,11 @@ describe FizzBuzz do
         expect(subject.check(3)).to eq "fizz"
     end
 
-    xit "is expected to return 'buzz' if the number is divisible by 5" do
+    it "is expected to return 'buzz' if the number is divisible by 5" do
         expect(subject.check(5)).to eq "buzz"
     end
 
-    xit "is expected to return 'fizz-buzz' if the number is divisible by 15" do
+    it "is expected to return 'fizz-buzz' if the number is divisible by 15" do
         expect(subject.check(15)).to eq "fizz-buzz"
     end
 
--- src/fizz_buzz.rb
+++ src/fizz_buzz.rb
@@ -1,7 +1,16 @@
 class FizzBuzz
     
     def check(value)
-        return value
+
+        if value % 15 == 0
+            return "fizz-buzz"
+        elsif value % 3 == 0
+            return "fizz"
+        elsif value % 5 == 0
+            return "buzz"
+        else
+            return value
+        end
     end
 
 end
