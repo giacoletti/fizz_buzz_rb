0c9a7c009825d241f366d3aa9885b6433729288d
giacoletti <giacoletti92@gmail.com>
2021-10-21 10:51:53 +0200

    first test for returning number only passed

--- spec/fizz_buzz_spec.rb
+++ spec/fizz_buzz_spec.rb
@@ -1,8 +1,9 @@
+require_relative '../src/fizz_buzz.rb'
+
 describe FizzBuzz do
 
     subject { FizzBuzz.new } #subject case
 
-    #test case
     it "is expected to return a number if none of the game rules apply" do
         expect(subject.check(1)).to eq 1
     end
@@ -11,11 +12,11 @@ describe FizzBuzz do
         expect(subject.check(3)).to eq "fizz"
     end
 
-    it "is expected to return 'buzz' if the number is divisible by 5" do
+    xit "is expected to return 'buzz' if the number is divisible by 5" do
         expect(subject.check(5)).to eq "buzz"
     end
 
-    it "is expected to return 'fizz-buzz' if the number is divisible by 15" do
+    xit "is expected to return 'fizz-buzz' if the number is divisible by 15" do
         expect(subject.check(15)).to eq "fizz-buzz"
     end
 
